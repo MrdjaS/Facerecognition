7d82a771a526da5736f672999b2f19cb70c104c3
MrdjaS <mrdjas96@hotmail.com>
2021-10-28 20:20:33 +0200

    Dodavanje funkcionalnosti stranici

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Particles from 'react-particles-js';
+import Clarifai, { COLOR_MODEL } from 'clarifai';
 import Navigation from './components/navigation/navigation';
+import facerecognition from './components/facerecognition/facerecognition';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: '4bb12e30667a4c6e8228a03cc5e8d7d0'
+});
 
 const particlesOptions = {
     particles: {
@@ -20,6 +25,30 @@ const particlesOptions = {
 }
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict(Clarifai.COLOR_MODEL, "https://samples.clarifai.com/face-det.jpg").then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+
+      }
+    );
+      
+  }
+
   render() {
     return (
       <div className="App">
@@ -28,8 +57,8 @@ class App extends React.Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition />*/}
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <faceRecognition />
       </div>
     );
   }
---
+++ src/components/facerecognition/facerecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const facerecognition = () => {
+    return (
+        <div className='center'>
+            <img src={'https://samples.clarifai.com/face-det.jpg'} alt='' />
+        </div>
+    );
+}
+
+export default facerecognition;
